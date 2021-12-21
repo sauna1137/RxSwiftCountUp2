541c11e3d668f6b19362aacace7a19e37b1febca
sauna1137 <subscription.search@gmail.com>
2021-12-21 18:09:16 +0900

    RxSwift追加 メソッド完成

--- RxSwiftCountUp2/ViewController.swift
+++ RxSwiftCountUp2/ViewController.swift
@@ -6,14 +6,53 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var countDownButton: UIButton!
+
+
+    private let count: BehaviorRelay<Int> = BehaviorRelay(value: 0)
+    private let dispose = DisposeBag()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        bindButtonToValue()
+        bindCountToText()
+        bindCountDownButtonToValue()
     }
 
+    private func bindButtonToValue() {
+        button.rx.tap
+            .subscribe(onNext: {[weak self] _ in
+                self?.increment()})
+            .disposed(by: dispose)
+    }
 
+    private func bindCountDownButtonToValue() {
+        countDownButton.rx.tap
+            .subscribe(onNext: {[weak self] _ in
+                self?.downCount()})
+            .disposed(by: dispose)
+    }
+
+    private func increment() {
+         count.accept(count.value + 1)
+     }
+
+    private func downCount() {
+        count.accept(count.value - 1)
+    }
+
+    private func bindCountToText() {
+        count.asObservable()
+            .subscribe(onNext: {[weak self] count in
+                self?.label.text = String(count) })
+            .disposed(by: dispose)
+    }
 }
-
